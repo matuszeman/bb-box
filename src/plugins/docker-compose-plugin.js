097409777ab94046cbf6801e444c70a00b9a791e
Matus Zeman <matus.zeman@gmail.com>
2017-11-08 17:11:49 +0100

    docker-compose plugin refactored

--- src/plugins/docker-compose-plugin.js
+++ src/plugins/docker-compose-plugin.js
@@ -1,76 +1,16 @@
 const {AbstractService, Joi} = require('@kapitchi/bb-service');
+const DockerComposeRuntime = require('./docker-compose-runtime');
 const YAML = require('yamljs');
 const _ = require('lodash');
-const shell = require('shelljs');
-const Docker = require('dockerode');
-const { spawnSync } = require('child_process');
 
 class DockerComposePlugin extends AbstractService {
   constructor() {
     super();
-
-    //check if docker-compose is available on local system
-    this.shell.exec('docker-compose --version', {silent: true});
-
-    this.docker = new Docker();
+    this.runtime = new DockerComposeRuntime();
   }
 
   register(box) {
-    box.runtimes['docker-compose'] = this;
-  }
-
-  async run(params) {
-    this.params(params, {
-      service: Joi.object(),
-      op: Joi.string()
-    });
-
-    //we don't do `params = this.params(...)` as we want original reference of the service
-    const {service, op} = params;
-
-    switch(op) {
-      case 'install':
-      case 'update':
-      case 'reset':
-        this.logger.log({
-          level: 'info',
-          msg: `DockerComposePlugin[${service.dockerImageName}]: START`
-        });
-
-        this.spawn('docker-compose', ['run', '--rm', service.dockerImageName, 'bb-box', op], {
-          env: service.env
-        });
-
-        this.logger.log({
-          level: 'info',
-          msg: `DockerComposePlugin[${service.dockerImageName}]: END`
-        });
-        break;
-      case 'start':
-        this.spawn('docker-compose', ['up', '-d', service.dockerImageName], {
-          env: service.env
-        });
-        break;
-      case 'stop':
-        this.spawn('docker-compose', ['stop', service.dockerImageName], {
-          env: service.env
-        });
-        break;
-      case 'status':
-        const containers = await this.docker.listContainers({
-          all: 1
-        });
-        const container = _.find(containers, (cnt) => {
-          return cnt.Labels['com.docker.compose.service'] === service.dockerImageName;
-        });
-        service.status = undefined;
-        if (container) {
-          service.status = container.State;
-        }
-        break;
-      default:
-        throw new Error('DockerComposePlugin: Not supported operation ' + op);
-    }
+    box.runtimes['docker-compose'] = this.runtime;
   }
 
   discoverServices() {
@@ -82,7 +22,9 @@ class DockerComposePlugin extends AbstractService {
 
         const def = _.omitBy({
           name: serviceName,
-          dockerImageName: serviceName,
+          dockerCompose: {
+            service: serviceName
+          },
           runtime: 'docker-compose',
           dependsOn: service.depends_on
         }, _.isUndefined);
@@ -108,25 +50,6 @@ class DockerComposePlugin extends AbstractService {
       }
       return {};
     }
-  }
-
-  spawn(cmd, args, opts) {
-    const ret = spawnSync(cmd, args, _.defaults({
-      stdio: 'inherit',
-      shell: true
-    }, opts));
-    if (ret.status !== 0) {
-      console.log(ret); //XXX
-      throw new Error('spawn error');
-    }
-  }
-
-  get shell() {
-    shell.config.reset();
-    shell.config.silent = true;
-    shell.config.fatal = true;
-
-    return shell;
   }
 }
 
---
+++ src/plugins/docker-compose-runtime.js
@@ -0,0 +1,93 @@
+const {AbstractService, Joi} = require('@kapitchi/bb-service');
+const _ = require('lodash');
+const shell = require('shelljs');
+const Docker = require('dockerode');
+const { spawnSync } = require('child_process');
+
+class DockerComposeRuntime extends AbstractService {
+  constructor() {
+    super();
+
+    //check if docker-compose is available on local system
+    this.shell.exec('docker-compose --version', {silent: true});
+
+    this.docker = new Docker();
+  }
+
+  async run(params) {
+    this.params(params, {
+      service: Joi.object(),
+      op: Joi.string()
+    });
+
+    //we don't do `params = this.params(...)` as we want original reference of the service
+    const {service, op} = params;
+
+    const serviceName = service.dockerCompose.service;
+
+    switch(op) {
+      case 'install':
+      case 'update':
+      case 'reset':
+        this.logger.log({
+          level: 'info',
+          msg: `DockerComposePlugin[${serviceName}]: START`
+        });
+
+        this.spawn('docker-compose', ['run', '--rm', serviceName, 'bb-box', op], {
+          env: service.env
+        });
+
+        this.logger.log({
+          level: 'info',
+          msg: `DockerComposePlugin[${serviceName}]: END`
+        });
+        break;
+      case 'start':
+        this.spawn('docker-compose', ['up', '-d', serviceName], {
+          env: service.env
+        });
+        break;
+      case 'stop':
+        this.spawn('docker-compose', ['stop', serviceName], {
+          env: service.env
+        });
+        break;
+      case 'status':
+        const containers = await this.docker.listContainers({
+          all: 1
+        });
+        const container = _.find(containers, (cnt) => {
+          return cnt.Labels['com.docker.compose.service'] === serviceName;
+        });
+        service.status = undefined;
+        if (container) {
+          service.status = container.State;
+        }
+        break;
+      default:
+        throw new Error('DockerComposePlugin: Not supported operation ' + op);
+    }
+  }
+
+  spawn(cmd, args, opts) {
+    const ret = spawnSync(cmd, args, _.defaults({
+      stdio: 'inherit',
+      shell: true
+    }, opts));
+    if (ret.status !== 0) {
+      console.log(ret); //XXX
+      throw new Error('spawn error');
+    }
+  }
+
+  get shell() {
+    shell.config.reset();
+    shell.config.silent = true;
+    shell.config.fatal = true;
+
+    return shell;
+  }
+}
+
+module.exports = DockerComposeRuntime;
