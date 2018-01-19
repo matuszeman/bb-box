e3f45adac53be8781d7bcb2bd9fb8f2146c46df2
Matus Zeman <matus.zeman@gmail.com>
2018-01-19 07:38:35 +0100

    console.debug -> console.log

--- bin/bbox.js
+++ bin/bbox.js
@@ -2,9 +2,9 @@
 var semver = require('semver');
 
 if (semver.gte(process.version, '7.6.0')) {
-  console.debug('Running from src'); //XXX
+  console.log('Running from src'); //XXX
   require('../src/bin/bb-box');
 } else {
-  console.debug('Running from es5'); //XXX
+  console.log('Running from es5'); //XXX
   require('../es5/bin/bb-box');
 }
